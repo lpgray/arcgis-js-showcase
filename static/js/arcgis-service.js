b66cb1391f44f19f4a1b8f1075f0675ca2ce9843
lpgray <rayzy1991@gmail.com>
2013-08-02 17:22:43 +0800

    use the map online.

--- static/js/arcgis-service.js
+++ static/js/arcgis-service.js
@@ -1,12 +1,21 @@
 dojo.require("esri.map");
 
-var map, dynamicMapServiceLayer, layerId;
+var map;
 function init() {
-	map = new esri.Map("map");
-    
-    layerId = "http://192.168.0.202:6080/arcgis/rest/services/ganyu/TDLYXZ_H_320721_2012/MapServer";
-    dynamicMapServiceLayer = new esri.layers.ArcGISDynamicMapServiceLayer(layerId);
-    
-    map.addLayer(dynamicMapServiceLayer);
+	map = new esri.Map("map", {
+		zoom : 5,
+		slider:false, 
+		nav:false,
+		center : [112.2, 32.8]
+	});
+	var basemapUrl= "http://www.arcgisonline.cn/ArcGIS/rest/services/ChinaOnlineStreetWarm/MapServer"
+	var basemap = new esri.layers.ArcGISTiledMapServiceLayer(basemapUrl);
+	map.addLayer(basemap);
+	
+	function myOnClickHandler(  ){
+		console.log(arguments);
+	}
+	
+	var myOnClick_connect = dojo.connect(map, "onClick", myOnClickHandler);
 }
-dojo.ready(init);
+dojo.addOnLoad(init);
