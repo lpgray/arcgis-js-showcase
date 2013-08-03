7cdce340144538b969335d9bec1d28e029b68b40
unknown <ray@.(none)>
2013-08-03 10:58:12 +0800

    zoomslider style change, map nav impl.

--- static/js/arcgis-service.js
+++ static/js/arcgis-service.js
@@ -1,21 +1,32 @@
 dojo.require("esri.map");
 
+
+
 var map;
 function init() {
 	map = new esri.Map("map", {
-		zoom : 5,
-		slider:false, 
-		nav:false,
-		center : [112.2, 32.8]
+		minZoom : 11 ,
+		logo : false ,
+		sliderStyle : 'large'
 	});
-	var basemapUrl= "http://www.arcgisonline.cn/ArcGIS/rest/services/ChinaOnlineStreetWarm/MapServer"
-	var basemap = new esri.layers.ArcGISTiledMapServiceLayer(basemapUrl);
-	map.addLayer(basemap);
+	var nanjing= "http://www.arcgisonline.cn/ArcGIS/rest/services/ChinaCities_Community_BaseMap_CHN/NanJing_Community_BaseMap_CHN/MapServer"
+	var nanjingMap = new esri.layers.ArcGISTiledMapServiceLayer(nanjing);
 	
-	function myOnClickHandler(  ){
+	var shanghai = "http://www.arcgisonline.cn/ArcGIS/rest/services/ChinaCities_Community_BaseMap_CHN/ShangHai_Community_BaseMap_CHN/MapServer"
+	var shanghaiMap = new esri.layers.ArcGISTiledMapServiceLayer(shanghai);
+	map.addLayer(nanjingMap);
+	map.addLayer(shanghaiMap);
+	
+	function myOnClickHandler(){
 		console.log(arguments);
 	}
 	
 	var myOnClick_connect = dojo.connect(map, "onClick", myOnClickHandler);
+	
+	dojo.connect(map, "onLoad", function() {
+	  //map.disablePan();
+	  console.log(map.isPan + ' ' + map.getZoom());
+	  
+	});
 }
 dojo.addOnLoad(init);
