c57023c7b2d1ab5882f4e37c3c4047232591fc95
lpgray <rayzy1991@gmail.com>
2013-08-05 12:29:47 +0800

    identify impl.

--- static/js/Identify.js
+++ static/js/Identify.js
@@ -1,26 +1,28 @@
 dojo.require("esri.map");
 dojo.require("esri.tasks.query");
 
-var map, queryTask, query, symbol, infoTemplate;
+var map, identifyTask, identifyParams, symbol;
 function init() {
 	map = new esri.Map("map", {
 		logo : false ,
 		sliderStyle : 'large'
 	});
-	// var nanjing= "http://www.arcgisonline.cn/ArcGIS/rest/services/ChinaCities_Community_BaseMap_CHN/NanJing_Community_BaseMap_CHN/MapServer";
-	// var nanjingMap = new esri.layers.ArcGISTiledMapServiceLayer(nanjing);
 	
-	var cnxzbj = "http://192.168.0.202:6080/arcgis/rest/services/JCDL/XZFW/MapServer";
-	var cnxzbjMap = new esri.layers.ArcGISTiledMapServiceLayer(cnxzbj);
-	
-	// map.addLayer(nanjingMap);
-	map.addLayer(cnxzbjMap);
+	var JSINDEX = "http://192.168.0.202:6080/arcgis/rest/services/JCDL/XZFW/MapServer";
+	var JSMap = new esri.layers.ArcGISTiledMapServiceLayer(JSINDEX);
+	map.addLayer(JSMap);
 	
 	dojo.connect(map, "onLoad", function() {
 	  taskDefinition();
+	  infoWindowDefinition();
 	  symbolDefinition();
-	  infoTemplateDefinition();
+	  dojo.connect(map, "onClick", doIdentify);
 	});
+}
+
+function infoWindowDefinition(){
+	map.infoWindow.resize(450, 200);
+	map.infoWindow.setTitle('信息');
 }
 
 function symbolDefinition(){
@@ -31,43 +33,43 @@ function symbolDefinition(){
 }
 
 function taskDefinition(){
-	queryTask = new esri.tasks.QueryTask("http://192.168.0.202:6080/arcgis/rest/services/JCDL/XZFW/MapServer/10");
-	query = new esri.tasks.Query();
-	query.returnGeometry = true;
+	// parameters definition
+	identifyParams = new esri.tasks.IdentifyParameters();
+	
+	identifyParams.tolerance = 3;
+    identifyParams.returnGeometry = true;
+    identifyParams.layerIds = [11, 10]; // query 省级 and 市级
+    identifyParams.width  = map.width;
+    identifyParams.height = map.height;
+    
+    identifyTask = new esri.tasks.IdentifyTask("http://192.168.0.202:6080/arcgis/rest/services/JCDL/XZFW/MapServer");
 }
 
-function infoTemplateDefinition(){
-	infoTemplate = new esri.InfoTemplate("${Name_CHN}", "Name : ${Name_CHN}");
-}
-
-function querys(){
-	query.text = dojo.byId('words').value;
-	queryTask.execute(query, function(msg){
-		showResults(msg);
+function doIdentify( e ){
+	identifyParams.geometry = e.mapPoint;
+	identifyParams.mapExtent = map.extent;
+	identifyTask.execute(identifyParams, function(results){
+		showResult(e, results );
 	}, function(err){
-		console.error(msg);
+		console.error(err);
 	});
 }
 
-
-function showResults(myFeatureSet) {
-	// console.debug(myFeatureSet);
-	
+function showResult( e, results ){
 	map.graphics.clear();
-	
-	var resultFeatures = myFeatureSet.features;
-	
-	for (var i=0, il=resultFeatures.length; i<il; i++) {
-      //Get the current feature from the featureSet.
-      //Feature is a graphic
-      var graphic = resultFeatures[i];
-      graphic.setSymbol(symbol);
-      //Set the infoTemplate.
-      graphic.setInfoTemplate(infoTemplate);
-      //Add graphic to the map graphics layer.
-      map.graphics.add(graphic);
-      console.debug(graphic);
-    }
+	var feature = results[0].feature;
+	feature.setSymbol(symbol);
+	map.graphics.add(feature);
+	map.infoWindow.setContent(formatContent( feature.attributes ));
+	map.infoWindow.show(e.screenPoint, map.getInfoWindowAnchor(e.screenPoint));
+}
+
+function formatContent( attributes ){
+	var back = '';
+	for( name in attributes ){
+		back += '<strong>' + name + ': </strong>' + attributes[name] + '<br />';
+	}
+	return back;
 }
 
 dojo.addOnLoad(init);
