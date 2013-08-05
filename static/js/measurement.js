44170d81126c32f0f3ab1a422c4191b446c40e92
lpgray <rayzy1991@gmail.com>
2013-08-05 16:55:53 +0800

    simplify measurement test case.

--- static/js/measurement.js
+++ static/js/measurement.js
@@ -1,13 +1,9 @@
 dojo.require('esri.map');
 dojo.require("esri.dijit.Measurement");
 
-var map, symbol;
+var map;
 
 function init(){
-	esri.config.defaults.io.proxyUrl = "/proxy";
-    esri.config.defaults.io.alwaysUseProxy = false;
-	esri.config.defaults.geometryService = new esri.tasks.GeometryService("http://tasks.arcgisonline.com/ArcGIS/rest/services/Geometry/GeometryServer");
-	
 	var tileMap = new esri.layers.ArcGISTiledMapServiceLayer("http://www.arcgisonline.cn/ArcGIS/rest/services/ChinaCities_Community_BaseMap_CHN/NanJing_Community_BaseMap_CHN/MapServer");
 	
 	map = new esri.Map("map", {
